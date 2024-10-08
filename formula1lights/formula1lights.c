53b3b3c30b26365ccc75560964d4d3f3ea0abf98
JAY♡ <jamieoconno@gmail.com>
2024-10-08 13:55:11 +0100

    Updated formula1lights.c
    Fixes the function to be called at the end. Updated comments.

--- formula1lights/formula1lights.c
+++ formula1lights/formula1lights.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <unistd.h>  // for sleep() function
 
-// Function to print the binary representation of a variable
+// Prototype function to print the binary representation of a variable
+void print_lights(unsigned char lights);
+
+int main() {
+    //Declare Variables
+    unsigned char lights = 0;  // This will store the state of the lights
+    char start_signal;
+    int i;
+    
+    //Prompt user to press 'S'
+    printf("Press 'S' to start the race sequence: ");
+    scanf(" %c", &start_signal);
+    
+    if (start_signal == 'S' || start_signal == 's') {
+        // Turn on each light with a 1-second delay
+        for (i = 0; i < 5; i++) {
+            lights |= (1 << i);  // Set the i'th bit to 1
+            print_lights(lights); // Print the current state of lights
+            sleep(1);  // Wait for 1 second
+        }
+        
+        // Waits 1 more second before turning off all lights
+        sleep(1);
+        printf("Start of the race! Lights off.\n");
+        
+        // Turn off all lights (set all lower 5 bits to 0)
+        lights = 0;
+        print_lights(lights);  // Print the current state of lights (all off to start race)
+    } else {
+        printf("Invalid input. Please press 'S' to start.\n"); //Notify user of invalid input
+    }
+    
+    return 0;
+}
+
+
+//Function to print the binary representation of a variable
 void print_lights(unsigned char lights) {
     // Print only the lower 5 bits
     int i;
@@ -11,35 +47,4 @@ void print_lights(unsigned char lights) {
         }
     }
     printf("\n");
-}
-
-int main() {
-    unsigned char lights = 0;  // This will store the state of the lights
-    char start_signal;
-    int i;
-    
-    // Wait for the user to press 'S'
-    printf("Press 'S' to start the race sequence: ");
-    scanf(" %c", &start_signal);
-    
-    if (start_signal == 'S' || start_signal == 's') {
-        // Turn on each light with a 1-second delay
-        for (i = 0; i < 5; i++) {
-            lights |= (1 << i);  // Set the ith bit to 1
-            print_lights(lights); // Print the current state of lights
-            sleep(1);  // Wait for 1 second
-        }
-        
-        // Wait 1 more second before turning off all lights
-        sleep(1);
-        printf("Start of the race! Lights off.\n");
-        
-        // Turn off all lights (set all lower 5 bits to 0)
-        lights = 0;
-        print_lights(lights);  // Print the current state of lights (all off)
-    } else {
-        printf("Invalid input. Please press 'S' to start.\n");
-    }
-    
-    return 0;
 }
